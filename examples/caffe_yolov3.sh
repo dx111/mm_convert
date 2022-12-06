cafbf907a928eb798438752e487b5e6f63109bb8
duanxiang <duanxiang@cambricon.com>
2022-12-06 06:51:23 +0000

    fix yolov3

--- examples/caffe_yolov3.sh
+++ examples/caffe_yolov3.sh
@@ -7,7 +7,7 @@ wget -c https://github.com/dx111/models/raw/main/caffe_yolov3/yolov3_416.prototx
 mm_convert \
     -f caffe \
     --proto models/caffe_yolov3/yolov3_416.prototxt \
-    --caffemodel models/caffe_yolov3/yolov3_416.caffemodel \
+    --model models/caffe_yolov3/yolov3_416.caffemodel \
     --output_model caffe_yolov3_model \
     --archs mtp_372.41 \
     --input_shapes 1,3,416,416 \
