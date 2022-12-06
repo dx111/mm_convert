9b9d1c636f0074c3f4c94d61cc0e97632c7ac3ea
duanxiang <duanxiang@cambricon.com>
2022-12-06 03:01:20 +0000

    fix

--- examples/caffe_yolov3.sh
+++ examples/caffe_yolov3.sh
@@ -9,12 +9,11 @@ mm_convert \
     --proto models/caffe_yolov3/yolov3_416.prototxt \
     --caffemodel models/caffe_yolov3/yolov3_416.caffemodel \
     --output_model caffe_yolov3_model \
-    --archs mtp_322 mtp_372.41 \
+    --archs mtp_372.41 \
     --input_shapes 1,3,416,416 \
     --input_as_nhwc true \
     --insert_bn true \
-    --precision qint8_mixed_float16 \
-    --calib_data_dir sample_data/coco \
+    --precision q8 \
     --image_color bgr \
     --image_std 1/0.00392,1/0.00392,1/0.00392 \
     --add_detect true
